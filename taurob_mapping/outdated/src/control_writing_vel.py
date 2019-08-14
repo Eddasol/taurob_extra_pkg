9b44f67d66c3a0d2e86a75365cc19e30ac0c2a66
Edda Solem <eddas@stud.ntnu.no>
2019-08-14 08:45:43 +0200

    Rydding

---
+++ taurob_mapping/outdated/src/control_writing_vel.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+'''
+Controlscript that makes it possible to override a node that desires to control velocity. It forwards the 
+desired velocity of the topic that is given control.
+'''
+import rospy
+from geometry_msgs.msg import Twist
+from std_msgs.msg import String
+
+# Possible topics that can deside the velocity
+DEFAULT = "cmd_vel_default"
+OVERRIDE = "cmd_vel_override"
+
+
+# Which of the topics that get to control the velocity at the current time
+writer = DEFAULT
+
+# Publisher where the velocity get published
+vel_pub = None
+
+def update_state(data):
+	# data.data is a string with the topic which should be in control
+	global writer
+	if writer != data.data:
+		print("Writer is updated from: ", writer, "to: ", data.data)
+
+		vel_pub.publish(Twist())
+
+	if data.data == OVERRIDE:
+		writer = OVERRIDE
+	else:
+		writer = DEFAULT
+
+
+def default_writer(velocity):
+	# Pases on the velocity from default velocity topic if it's choosen as the writer
+	if writer != DEFAULT:
+		return
+	
+	vel_pub.publish(velocity)
+
+
+def override_writer(velocity):
+	# Pases on the velocity from override velocity topic if it's choosen as the writer
+	if writer != OVERRIDE:
+		return
+	
+	vel_pub.publish(velocity)
+
+
+def main():
+    global vel_pub
+    rospy.init_node('recovery')
+    rospy.Subscriber("navigation_status", String, update_state) 
+    rospy.Subscriber(DEFAULT, Twist, default_writer)
+    rospy.Subscriber(OVERRIDE, Twist, override_writer)
+    vel_pub = rospy.Publisher("taurob_tracker/cmd_vel_raw", Twist, queue_size = 10)
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    main()
